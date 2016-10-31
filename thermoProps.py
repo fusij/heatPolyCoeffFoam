88fb1bde2e331a2aafa519e8a462157e56bb17b2
fusij <vigfusjosefsson@gmail.com>
2016-10-31 00:15:14 +0000

    added print out of polyCoeffs. Explicit declaration of variables, need to add function call with command-line arguments

--- thermoProps.py
+++ thermoProps.py
@@ -4,6 +4,7 @@
 import sys
 import numpy as np
 from CoolProp.CoolProp import PropsSI
+import matplotlib.pyplot as plt
 
 if len(sys.argv) != 6:
     print "usage: <fluid> <min temperature [°C]> <max temperature [°C]>  <pressure [bar]>, <no. of points in range>"
@@ -24,12 +25,46 @@ def thermoProps(fluid, minTemp, maxTemp, pressure, points):
     for i, value in enumerate(tempRange):
         density[i] = PropsSI('D','T',value,'P',pressure,str(fluid)) #kg/m3
         viscosity[i] = PropsSI('V','T',value,'P',pressure,str(fluid)) #Pa*s
-        specHeat[i] = PropsSI('D','T',value,'P',pressure,str(fluid)) #J/(kg*K)
+        specHeat[i] = PropsSI('C','T',value,'P',pressure,str(fluid)) #J/(kg*K)
         
-    print density
-    return density, viscosity, specHeat
+    return tempRange,density, viscosity, specHeat
 
-#thermoProps(fluid, minTemp, maxTemp, pressure, points)
+def curveFit(tempRange, thermoProp, degree):
+    polyCoeff = np.polyfit(tempRange, thermoProp, degree)
+
+    return polyCoeff
+
+tempRange, density, viscosity, specHeat = thermoProps(fluid, minTemp, maxTemp, pressure, points)
+#print density, viscosity, specHeat
+
+
+densFit = np.poly1d(np.polyfit(tempRange, density,6))
+viscFit = np.poly1d(np.polyfit(tempRange, viscosity,6))
+specHeatFit = np.poly1d(np.polyfit(tempRange, specHeat,6))
+
+print np.polyfit(tempRange, density,6)
+print np.polyfit(tempRange, viscosity,6)
+print np.polyfit(tempRange, specHeat,6)
+
+#plt.figure()
+plt.plot(tempRange, density, label='1')
+plt.plot(tempRange, densFit(tempRange), label='fit')
+plt.legend() 
+plt.show()
+plt.plot(tempRange, viscosity, label='2')
+plt.plot(tempRange, viscFit(tempRange), label='fit')
+plt.legend() 
+plt.show()
+plt.plot(tempRange, specHeat, label='3')
+plt.plot(tempRange, specHeatFit(tempRange), label='fit')
+plt.legend()
+plt.show()
+
+
+
+
+
+
 
 
 
